eefb01f1a3fc830adedd98263650bc9b410b37a9
thomas01111 <luccruz@uade.edu.ar>
2022-12-04 14:59:20 -0300

    formulario-listo

--- formulario-cafeTour.js
+++ formulario-cafeTour.js
@@ -1,13 +1,19 @@
-const edad = document.getElementById("edad");
-const nombre = document.getElementById("nombre");
-const email = document.getElementById("email");
-const apellido = document.getElementById("apellido");
+window.addEventListener("load", function(){
+    this.alert("cargando formulario por favor espere");
+
+    let formulario = document.querySelector("form.formulario");
+    let edad = document.querySelector("input.edad");
+    let botonEnviar = document.getElementById("botonEnviar")
+
+    formulario.addEventListener("submit", function(e){
+        if(edad.value<=0){
+            this.alert("En el campo edad...No se puede ingresar valores negativos");
+        }
+    })
+
+    botonEnviar.addEventListener('click', function(){
+        window.confirm("¿Estás seguro que queres enviar el formulario?");
+    })
+})
 
 
-
-
-
-function enviar(){
-    window.confirm("¿Estás seguro que queres enviar el formulario?");
-    
-}
--- formulario-sobreNosotros.js
+++ formulario-sobreNosotros.js
@@ -2,16 +2,17 @@ const nombre = document.getElementById("nombre")
 const apellido = document.getElementById("apellido")
 const email = document.getElementById("email")
 const botonEnviar = document.getElementById("boton")
+
 console.log(nombre , apellido , email)
 
-window.addEventListener("load",function(){
+window.addEventListener("load",function(e){
     this.alert("cargando formulario por favor espere")
-   
+    botonEnviar.addEventListener('click', function(){
+        window.confirm("¿Estás seguro que queres enviar el formulario?");
+    })
+    
 })
 
 
-function enviar(){
-    window.confirm("¿Estás seguro que queres enviar el formulario?");
-    
-}
+
 
